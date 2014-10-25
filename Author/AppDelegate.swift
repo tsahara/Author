4fc2c124638a67f3b4e76111023766ae778cd571
Tomoyuki Sahara <sahara@caddr.net>
2014-10-25 14:17:39 +0900

    Initial Commit

---
+++ Author/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Author
+//
+//  Created by Tomoyuki Sahara on 10/25/14.
+//  Copyright (c) 2014 Tomoyuki Sahara. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
